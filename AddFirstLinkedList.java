e933d0c8ae533abb1af59abf2704539a3ce9379e
Nivas Ganesan <38051335+Nivas138@users.noreply.github.com>
2021-01-30 16:35:31 +0530

    Add First and Last in Linked List

---
+++ AddFirstLinkedList.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class Node{
+    int data;
+    Node next;
+}
+public class MyClass {
+    Node head;
+    
+    public void addNodeFirst(int value){
+        Node newNode = new Node();
+        newNode.data = value;
+        newNode.next = head; 
+        head = newNode;
+    }
+    
+    public void addNodeLast(int value){
+        
+        Node newNode = new Node();
+        newNode.data = value;
+        newNode.next = null;
+        
+        if(head == null){
+            head = newNode;
+        }else{
+            Node lastNode = head;
+            
+            while(lastNode.next != null){
+                lastNode = lastNode.next;
+            }
+            
+            lastNode.next = newNode;
+        }
+    }
+    
+    public void printList(){
+        Node temp = head;
+        while(temp != null){
+            System.out.println("--Elemet : "+temp.data);
+            temp = temp.next;
+        }
+    }
+    
+    public static void main(String[] args){
+        MyClass singlyLinkedlist = new MyClass();
+        System.out.println("---LinkedList---");
+        singlyLinkedlist.addNodeFirst(10);
+        singlyLinkedlist.addNodeFirst(20);
+        singlyLinkedlist.addNodeFirst(40);
+        singlyLinkedlist.addNodeLast(30);
+        singlyLinkedlist.addNodeLast(15);
+        singlyLinkedlist.printList();
+    }
+}
